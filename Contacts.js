379c5dc7b28db626803e9c4bccb3a5de254546e7
Traxine <traxine2010@hotmail.com>
2016-11-08 14:41:13 -0600

    More knockout added

--- Contacts.js
+++ Contacts.js
@@ -1,6 +1,10 @@
 ﻿$(function () {
     function viewModel() {
-        this.authorName = ko.observable('Steve Smith');
+        this.firstName = ko.observable('Steve');
+        this.lastName = ko.observable('Smith');
+        this.fullName = ko.computed(function () {
+            return this.firstName() + " " + this.lastName();
+        }, this)
         this.twitterAlias = ko.observable('@fu');
         this.twitterUrl = ko.computed(function () {
             return "https://twitter.com/" + this.twitterAlias().replace('@', '');
